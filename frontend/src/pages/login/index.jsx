3e5fb465efe690eab294de5c7a4f693a52f33528
root_who <joaopaulo_gonzaga@outlook.com>
2021-11-20 06:05:00 -0300

    arrumando detalhes finais fixing more

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -1,6 +1,6 @@
 import Alert from 'react-bootstrap/Alert'
 import React from 'react';
-
+import { CircularProgress} from '@material-ui/core';
 import '../../assets/css/login/login.css'
 import InputFormCadastro from '../../components/inputFormCadastro';
 import validaForm from '../../util/validacoes';
@@ -9,6 +9,7 @@ import API_URL from '../../util/API/apiRequest';
 function LoginPage() {
     const [senha, setSenha] = React.useState("");
     const [email, setEmail] = React.useState("");
+    const [request, setRequest] = React.useState(false);
     const [user, setUser] = React.useState({});
     const [alertMessaage, setAlertMessage] = React.useState({
         alertView: false,
@@ -21,40 +22,46 @@ function LoginPage() {
         senha: { hasErros: true, classValid: "", message: "" }
     })
 
-    function formSubmit(event){
+    function formSubmit(event) {
         event.preventDefault();
-        if(validaForm.validacaoLogin(errors)){
-            setAlertMessage({...alertMessaage,
-            text:"Erro em algum campo do formulario, verifique e corrija antes de enviar!",
-            color: "danger",
-            alertView:true
+        if (validaForm.validacaoLogin(errors)) {
+            setAlertMessage({
+                ...alertMessaage,
+                text: "Erro em algum campo do formulario, verifique e corrija antes de enviar!",
+                color: "danger",
+                alertView: true
             })
-        }else{
+        } else {
+            setRequest(true)
             axios.request({
-                    method: 'post',
-                    url: API_URL + '/cliente/login',
-                    data: {
-                        "senha" : senha ,
-                        "email" : email,
-                    }
-                }).then((response)=>{
-                     setAlertMessage({...alertMessaage,
-                        text: response.data.erro.errorText,
-                        color:  response.data.erro.color,
-                        hasError: response.data.erro.hasError,
-                        alertView: true})
-                    if(!alertMessaage.hasError){
-                        console.log(response.data)
-                        setUser({...user, nome: response.data.cliente.nome, id: response.data.cliente.id})
-                        localStorage.setItem('user', JSON.stringify({nome: response.data.cliente.nome, id: response.data.cliente.id, cestaId : response.data.cestaId}));
-                    }     
-                }).catch(()=>{
-                    setAlertMessage({...alertMessaage,
-                        text:"Erro na conexão tente novamente mais tarde!",
+                method: 'post',
+                url: API_URL + '/cliente/login',
+                data: {
+                    "senha": senha,
+                    "email": email,
+                }
+            }).then((response) => {
+                setAlertMessage({
+                    ...alertMessaage,
+                    text: response.data.erro.errorText,
+                    color: response.data.erro.color,
+                    hasError: response.data.erro.hasError,
+                    alertView: true
+                })
+                if(response.status !== 200 || response.status !== 400){
+                    setAlertMessage({
+                        ...alertMessaage,
+                        text: "Erro na conexão tente novamente mais tarde!",
                         color: "warning",
-                        alertView:true
-                        })
-                }) 
+                        alertView: true
+                    })
+                }
+                if (!alertMessaage.hasError) {
+                    console.log(response.data)
+                    setUser({ ...user, nome: response.data.cliente.nome, id: response.data.cliente.id })
+                    localStorage.setItem('user', JSON.stringify({ nome: response.data.cliente.nome, id: response.data.cliente.id, cestaId: response.data.cestaId }));
+                }
+            })
         }
     }
 
@@ -63,46 +70,50 @@ function LoginPage() {
             <section class="conteudoAutentica">
                 <div class="cadastrado">
                     <h1>Já Tenho cadastro</h1>
-                    <Alert style={{height:"60px"}} show={alertMessaage.alertView} variant={alertMessaage.color} onClose={() =>  setAlertMessage({...alertMessaage, alertView: false})} dismissible>
+                    <Alert style={{ height: "60px" }} show={alertMessaage.alertView} variant={alertMessaage.color} onClose={() => setAlertMessage({ ...alertMessaage, alertView: false })} dismissible>
                         <Alert.Heading>{alertMessaage.text}</Alert.Heading>
                     </Alert>
-                    <form class="row g-3 needs-validation" style={{justifyContent: "flex-start"}} novalidate>
+                    <form class="row g-3 needs-validation" style={{ justifyContent: "flex-start" }} novalidate>
                         <InputFormCadastro onChangeValue={(event) => {
                             event.preventDefault();
                             setEmail(event.target.value);
                         }}
-                        onBlurInput={(event) => {
-                            setErrors({ ...errors, email: validaForm.validaEmail(email) })
-                        }}
-                        labelText={"Email"} 
-                        inputClassName={errors.email.classValid} 
-                        errorMessage={errors.email.message} 
-                        inputType={"text"} 
-                        inputSize={"8"}/>
+                            onBlurInput={(event) => {
+                                setErrors({ ...errors, email: validaForm.validaEmail(email) })
+                            }}
+                            labelText={"Email"}
+                            inputClassName={errors.email.classValid}
+                            errorMessage={errors.email.message}
+                            inputType={"text"}
+                            inputSize={"8"} />
                         <InputFormCadastro onChangeValue={(event) => {
                             event.preventDefault();
                             setSenha(event.target.value);
                         }}
-                        onBlurInput={(event) => {
-                            setErrors({ ...errors, senha: validaForm.validaSenha(senha) })
+                            onBlurInput={(event) => {
+                                setErrors({ ...errors, senha: validaForm.validaSenha(senha) })
 
-                        }}
-                        labelText={"Senha"} 
-                        inputClassName={errors.senha.classValid} 
-                        errorMessage={errors.senha.message} 
-                        inputType={"password"} 
-                        inputSize={"8"}/>
+                            }}
+                            labelText={"Senha"}
+                            inputClassName={errors.senha.classValid}
+                            errorMessage={errors.senha.message}
+                            inputType={"password"}
+                            inputSize={"8"} />
                         <div class="col-12">
-                            <button class="btn btn-primary" onClick={(event)=>{formSubmit(event)}}>Logar</button>
+                            <button class="btn btn-primary" onClick={(event) => { formSubmit(event) }}>Logar</button>
+                        </div>
+                        <div style={{ visibility: request ? "visible" : "hidden", margin: request ? "1.5rem 0 0 0" : "0 0", display: request ? "block" : "none" }}>
+                            <CircularProgress color="primary" />
                         </div>
                         <div class="col-12">
-                            <a href="./esqueci-senha" class="btn btn-secondary">Esqueci a senha</a>
+                            <a href="/esqueci-senha" class="btn btn-secondary">Esqueci a senha</a>
                         </div>
+
                     </form>
                 </div>
                 <div class="naoCadastrado">
                     <h1>Ainda não tenho Cadastro</h1>
-                    <a href="./cadastro" class="btn btn-secondary">Clique aqui</a>
+                    <a href="/cadastro" class="btn btn-secondary">Clique aqui</a>
                 </div>
 
             </section>
