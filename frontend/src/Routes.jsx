5c4a0160552fac8044a6660dd37126cadc7bf26c
root_who <joaopaulo_gonzaga@outlook.com>
2021-11-29 13:24:39 -0300

    updating...

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -6,8 +6,8 @@ import LoginPage from './pages/login';
 import DetalheProduto from './pages/produtoDetalhe';
 import CestaPage from './pages/cesta';
 import EsqueciSenhaPage from './pages/esqueciSenha';
-import HeaderComponent from './components/header';
-import FooterComponent from './components/footer';
+// import HeaderComponent from './components/header';
+// import FooterComponent from './components/footer';
 
 
 
