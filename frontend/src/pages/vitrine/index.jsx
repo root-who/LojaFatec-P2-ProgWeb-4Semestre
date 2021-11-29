0e2289d84c3e2978d18e8f91a51a276de4d6065f
root_who <joaopaulo_gonzaga@outlook.com>
2021-11-29 15:16:48 -0300

    circularprogess add in vitrine

--- frontend/src/pages/vitrine/index.jsx
+++ frontend/src/pages/vitrine/index.jsx
@@ -3,15 +3,18 @@ import React from "react";
 import '../../assets/css/vitrine/vitrine.css'
 import Produto from '../../components/produto';
 import API_URL from "../../util/API/apiRequest";
+import { CircularProgress} from '@material-ui/core';
 
 function VitrinePage() {
 
     const [produtos, setProdutos] = React.useState([])
+    const [request, setRequest] = React.useState(false)
 
     React.useEffect(()=>{
         if(JSON.parse(localStorage.getItem('produtos')) !== null){
             setProdutos(...produtos, JSON.parse(localStorage.getItem('produtos')))
         }else{
+            setRequest(true);
              axios.request({
                     method: 'GET',
                     url: API_URL + '/produtos/todos'
@@ -19,7 +22,8 @@ function VitrinePage() {
                         setProdutos(request.data)
                         console.log(request.data)
                         localStorage.setItem('produtos', JSON.stringify(request.data));
-                    })
+                        setRequest(false)
+                    }).catch((response)=>{setRequest(false)})
         }
         
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -36,9 +40,11 @@ function VitrinePage() {
                                 <Produto key={index} value={value}/>
                             )
                         })
-                }
-                
+                }   
             </main>
+            <div style={{ margin:"2.75rem 0 10rem 0", width:"100%", textAlign:"center", visibility: request ? "visible" : "hidden", display: request ? "block" : "none" }}>
+                 <CircularProgress color="primary" />
+            </div>
         </>
     )
 }
